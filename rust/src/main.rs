541f88986e5b6d4ac5193f6b1c2a2bcbbbc34e91
fofow <1760855795@qq.com>
2026-04-01 14:00:35 +0800

    feat: 最简单的Rust服务

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/",get(hello_handler));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3001").await.unwrap();
+    println!("Server already running on http://127.0.0.1:3001");
+    axum::serve(listener,app).await.unwrap();
+}
+
+async fn hello_handler() -> &'static str {
+    "Hello,Axum!"
 }
